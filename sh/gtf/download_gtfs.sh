936fb848fc9caa68754e23b80c108b156f6be40b
Anil Thanki <thanki.anil@gmail.com>
2025-06-06 14:34:04 +0100

    Update download_gtfs.sh - skipping WBPS_REL

--- sh/gtf/download_gtfs.sh
+++ sh/gtf/download_gtfs.sh
@@ -3,22 +3,22 @@
 # This script downloads, cleans up and coverts to gff3 format the gtf files as defined in the atlasprod/irap/gxa_references.conf
 set -euo pipefail
 
-if [ $# -lt 3 ]; then
-  echo "Usage: $0 ENSEMBL_REL ENSEMBLGENOMES_REL WBPS"
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 ENSEMBL_REL ENSEMBLGENOMES_REL"
   echo "e.g. $0 85 33 7"
   exit 1
 fi
 
 ENSEMBL_REL=$1
 ENSEMBLGENOMES_REL=$2
-WBPS_REL=$3
+# WBPS_REL=$3
 
 # The template to slot release numbers into
 gxaRefsTemplate=$(dirname ${BASH_SOURCE[0]})/gxa_references.conf
 # The config file that will be used for the actual download of the gtf files
 gxaRefs=/tmp/gxa_references.conf.$( eval date +%Y-%m-%d ).$$
 grep "ftp.ensemblgenomes.org" $gxaRefsTemplate | perl -p -e "s|RELNO|$ENSEMBLGENOMES_REL|g" > $gxaRefs
-grep "wormbase" $gxaRefsTemplate | perl -p -e "s|RELNO|$WBPS_REL|g" >> $gxaRefs
+# grep "wormbase" $gxaRefsTemplate | perl -p -e "s|RELNO|$WBPS_REL|g" >> $gxaRefs
 grep "ftp.ensembl.org" $gxaRefsTemplate | perl -p -e "s|RELNO|$ENSEMBL_REL|g" >> $gxaRefs
 
 # Directory in which all gtfs/gff3 files
